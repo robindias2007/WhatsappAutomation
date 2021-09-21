48a975d5ebb1d2e4dba22e56295cd2d7f0e28acc
robindias2007 <robindias2007@gmail.com>
2021-09-22 00:01:04 +0530

    python whatsapp auto

--- message.py
+++ message.py
@@ -4,12 +4,10 @@ account_sid = 'ACc6a79695d0bd0221b809a1d6f085c955'
 auth_token = '1fc1a8d386fac57016dfe43bca3428b1'
 client = Client(account_sid, auth_token)
 
-message = input(r'Enter message to be sent?')
-
 def send_message():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
-        body= message,
+        body= 'This is for testing. Have a nice day. I love you',
         to='whatsapp:+919833564323'
     )
 
